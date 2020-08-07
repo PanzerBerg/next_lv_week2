d5188c8dfd9dfa06b129eea2463c05483ac858c8
Matheus Santos <matheus.san99@gmail.com>
2020-08-07 16:01:31 -0300

    Aula 3 concluida

--- web/src/components/TeacherItem/index.tsx
+++ web/src/components/TeacherItem/index.tsx
@@ -1,35 +1,53 @@
 import React from 'react'
 
 import whatsappIcon from '../../assets/images/icons/whatsapp.svg'
+import api from '../../services/api'
 
 import './styles.css'
 
-const TeacherItem = () => {
+export interface Teacher {
+    id: number,
+    avatar: string,
+    bio: string,
+    cost: number,
+    name: string,
+    subject: string,
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+    
+    function createNewConnection() {
+        api.post('connections', {
+            user_id: teacher.id
+        })
+    }
+
     return (
         <article className="teacher-item">
             <header>
-                <img src="https://avatars2.githubusercontent.com/u/42724637?s=460&u=cdd72e4fcb664d17ecaa8efce4289a6de1e06d7d&v=4" alt="Matheus Santos" />
+                <img src={teacher.avatar} alt={teacher.name} />
                 <div>
-                    <strong>Matheus Santos</strong>
-                    <span>Química</span>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
                 </div>
             </header>
 
-            <p>
-                Entusiasta das melhores tecnologias de química avançada.
-                        <br /><br />
-                        Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.
-                    </p>
+            <p>{teacher.bio}</p>
 
             <footer>
                 <p>
                     Preço/Hora
-                            <strong>R$ 80,00</strong>
+    <strong>R$ {teacher.cost}</strong>
                 </p>
-                    <button type='button'>
-                        <img src={whatsappIcon} alt="Whatsapp" />
+                <a target='_blank' onClick={createNewConnection} href={`https://wa.me/${teacher.whatsapp}`} type='button'>
+                    <img src={whatsappIcon} alt="Whatsapp" />
                                     Entrar em contato
-                    </button>
+                    </a>
             </footer>
         </article>
     )
